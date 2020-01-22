84ccd636ee29e1a841e732a0fe4cef706946e588
Steven Zhu <stevenzhu03@gmail.com>
2020-01-22 17:59:36 -0500

    Yelp.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,42 @@
 require_relative '../config/environment'
+# require 'tty-prompt'
 
+def api_data(term, location)
+    data = Yelp.search(term, location)
+end
+
+# def search_restaurant_info
+# api_data
+
+#     data.map do |restaurant|
+#         attributes = {
+#             name: restaurant["name"]
+
+#         cusine_array = restaurant["categories"].map do |cuisine|
+#                        cuisine["title"]
+#                        end
+#             type: cuisine_types.join(",")
+#             location: restaurant["location"]["display_address"].join(" ")
+#             rating: restaurant["rating"]
+#         }
+#     end
+# end
+
+#Output options for restaurant selection
+
+def restaurant_options
+    prompt = TTY::Prompt.new
+
+    prompt.select('Which restaurant do you want to look into?') do |menu|
+        api_data.map do |restaurant|
+            menu.choice restaurant[:name], -> do
+                puts "Here is some info about #{restaurant[:name]}!"
+                puts "Type of Food: #{restaurant["categories"]}"
+                puts "Location: #{restaurant["location"]["display_address"].join(" ")}"
+                puts "Rating: #{restaurant["rating"]}"
+
+
+            end
+        end
+    end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,18 +3,8 @@ require "json"
 require "http"
 require "optparse"
 require 'bundler'
+require 'tty-prompt'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
-
-
-API_HOST = "https://api.yelp.com"
-SEARCH_PATH = "/v3/businesses/search"
-BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
-API_KEY = ENV["6SkpxfmOou1-J6vUmaqqFPT4wtmjGiK1lsue9ZkqUCLqrv6-9k5Hxfhu1yTifwiRoXmF3QZVISyCmBaF0cW4IizoiRzmiBWb_yzkJVFmincW5Csi5YqlYBqx3zQnXnYx"]
-
-DEFAULT_BUSINESS_ID = "yelp-san-francisco"
-DEFAULT_TERM = "dinner"
-DEFAULT_LOCATION = "New York, NY"
-SEARCH_LIMIT = 5
